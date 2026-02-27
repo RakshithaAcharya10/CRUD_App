b85f96fc302f09bc4a575160ddd935870f8e5ccf
Rakshitha Acharya <rakshithaacharya20@gmail.com>
2026-02-27 20:13:34 +0530

    CRUD APP

---
+++ client/src/Modules/Admin/ARoutes/AdminRoute.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import AHome from '../AComponents/AHome'
+import Sidebar from '../AComponents/Sidebar'
+export default function AdminRoute() {
+  return (
+    <div>
+        <Sidebar/>
+      <Routes>
+        <Route path='/AHome' element={<AHome/>}/>
+      </Routes>
+    </div>
+  )
+}
